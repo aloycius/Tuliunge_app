5337af088e0f9c889698d5d84e3cc723740126d5
aloycius <aloyciuslaurent58@gmail.com>
2024-10-28 14:22:54 +0300

    Working on the backend

--- tuliunge_backend/settings.py
+++ tuliunge_backend/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rides',
+    'authentication',
+    'user',
 ]
 
 MIDDLEWARE = [
