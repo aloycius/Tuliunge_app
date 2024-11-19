7f2c8cc2f0ffc7e9005ef5857550b06f2d34fea3
aloycius <aloyciuslaurent58@gmail.com>
2024-11-19 15:33:01 +0300

    Added the LogoutView in user/views.py

--- user/views.py
+++ user/views.py
@@ -5,14 +5,15 @@ from django.shortcuts import render
 # ...
 # Create your views here.
 # user/views.py
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import User
+from .models import UserManager
 from django.contrib.auth import authenticate
 from .serializer import RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.tokens import RefreshToken  # for JWT tokens
+from rest_framework.exceptions import ValidationError
 
 # Register API
 """"
@@ -71,6 +72,26 @@ class LoginView(APIView):
                 }, status=status.HTTP_200_OK)
             return Response({"msg": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#LOGOUT API
+    
+class LogoutView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            # The refresh token is expected to be provided in the request data
+            refresh_token = request.data.get("refresh")
+            if not refresh_token:
+                return Response({"msg": "Refresh token is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+            token = RefreshToken(refresh_token)
+            ##token.blacklist()  # Blacklist the token if you're using the blacklist app
+            return Response({"msg": "Successfully logged out."}, status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            raise ValidationError({"msg": "Token is invalid or expired."})
+
+    
+
 
     
 
